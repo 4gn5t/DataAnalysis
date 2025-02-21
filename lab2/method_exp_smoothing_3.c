f69aba8318a189c031c3b1226d43a3a45ccac954
4gn5t <zborivskyiworkingspace@gmail.com>
2025-02-21 16:45:22 +0000

    fix metrix

--- lab2/method_exp_smoothing_3.c
+++ lab2/method_exp_smoothing_3.c
@@ -52,16 +52,18 @@ void test_alpha(double* x_values, double* y_values, int count, double alpha) {
 int main() {
     int count = 0;
 
-    double* x_values = (double*)malloc(BUFFER_SIZE * sizeof(double));
-    double* y_values = (double*)malloc(BUFFER_SIZE * sizeof(double));
+    double* x_values_original = (double*)malloc(BUFFER_SIZE * sizeof(double));
+    double* y_values_original = (double*)malloc(BUFFER_SIZE * sizeof(double));
+    double* x_values_noise = (double*)malloc(BUFFER_SIZE * sizeof(double));
+    double* y_values_noise = (double*)malloc(BUFFER_SIZE * sizeof(double));
 
-    read_values_from_file("files/Funct_with_GausseNoise.txt", x_values, y_values, &count);
-
+    read_values_from_file("files/Funct_with_GausseNoise.txt", x_values_noise, y_values_noise, &count);
+    read_values_from_file("files/Funct_2.txt", x_values_original, y_values_original, &count);
     double alpha_values[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
     int num_alphas = sizeof(alpha_values) / sizeof(alpha_values[0]);
 
     for (int i = 0; i < num_alphas; i++) {
-        test_alpha(x_values, y_values, count, alpha_values[i]);
+        test_alpha(x_values_original, y_values_original, count, alpha_values[i]);
     }
     
     const double best_alpha = 0.1;
@@ -70,19 +72,21 @@ int main() {
     open_file(&Exp_smoothing, "files/Exp_smoothing.txt", "w");
 
     for (int i = 0; i < count; i++) {
-        double smoothed = method_exp_smoothing(y_values, i, best_alpha);
-        if (fprintf(Exp_smoothing, "%.2f %.4f\n", x_values[i], smoothed) < 0) {
+        double smoothed = method_exp_smoothing(y_values_noise, i, best_alpha);
+        if (fprintf(Exp_smoothing, "%.2f %.4f\n", x_values_noise[i], smoothed) < 0) {
             perror("Error writing to file");
             fclose(Exp_smoothing);
-            free(x_values);
-            free(y_values);
+            free(x_values_noise);
+            free(y_values_noise);
             return 1;
         }
     }
 
     fclose(Exp_smoothing);
-    free(x_values);
-    free(y_values);
+    free(x_values_original);
+    free(y_values_original);
+    free(x_values_noise);
+    free(y_values_noise);
 
     return 0;
 }
